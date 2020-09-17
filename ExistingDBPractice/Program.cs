0c3ed7a5cd4a09c34efede4d5a7d983dd072104d
Jia <jiavon@ualberta.ca>
2020-09-16 19:02:03 -0600

    First name search was success

--- ExistingDBPractice/Program.cs
+++ ExistingDBPractice/Program.cs
@@ -10,11 +10,12 @@ namespace ExistingDBPractice
             // Initialize our database interaction (the context):
             using(PersonContext context = new PersonContext())
             {
-                //string firstName;
-                //Console.WriteLine("Please enter a model to remove: ");
-                //firstName = Console.ReadLine().Trim().ToLower();
+                string firstName;
+                Console.WriteLine("Please enter the first name ");
+                firstName = Console.ReadLine().Trim().ToLower();
 
-                Console.WriteLine(context.Persons.Where(x => x.FirstName == "Tommy").SingleOrDefault());
+                Console.WriteLine("The person's full name is: ");
+                Console.WriteLine(context.Persons.Where(x => x.FirstName.ToLower() == firstName).SingleOrDefault().FirstName.ToString());
             }
         }
     }
